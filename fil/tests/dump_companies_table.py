b9e4e09755e31a3c432059f3bc15acaf230835b8
graxel <grazel98@gmail.com>
2024-06-14 17:01:57 -0400

    dump companies table

---
+++ fil/tests/dump_companies_table.py
@@ -0,0 +1,35 @@
+import os
+import psycopg2
+from psycopg2 import OperationalError, Error
+import pandas as pd
+
+def sql(query, data=None):
+    try:
+        conn = psycopg2.connect(
+            host=os.getenv('JOBSDB_HOST'),
+            database=os.getenv('JOBSDB_DATABASE'),
+            user=os.getenv('JOBSDB_USERNAME'),
+            password=os.getenv('JOBSDB_PASSWORD'))
+        cursor = conn.cursor()
+        if data is None:
+            cursor.execute(query)
+        else:
+            cursor.execute(query, data)
+        conn.commit()
+        if cursor.pgresult_ptr is not None:
+            rows = cursor.fetchall()
+            return rows
+    except Error as e:
+        print("Error: Unable to execute the query.")
+        print(e)
+    finally:
+        if cursor:
+            cursor.close()
+        if conn:
+            conn.close()
+    
+res = sql("""SELECT * FROM companies;""")
+res_list = list(res)
+
+df = pd.DataFrame(data=res_list, columns=['id', 'company_name', 'website_url', 'website_type', 'blacklisted'])
+df.to_csv('companies_seed.csv', index=False)
