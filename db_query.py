c871812820ad65928e34bc4cd7143d79975e87c3
graxel <grazel98@gmail.com>
2024-06-16 09:53:38 -0400

    less verbose query tools

--- db_query.py
+++ db_query.py
@@ -3,20 +3,16 @@ import fil
 import sys
 import pandas as pd
 
-for i,arg in enumerate(sys.argv):
-    print(i, arg)
 if len(sys.argv) < 2:
     print("Usage: python db_query.py <query or .sql file>")
 else:
     # Join all the arguments except the script name
     query_or_file = ' '.join(sys.argv[1:])
-    print(f'searching for {query_or_file}')
     if os.path.exists(query_or_file):
         with open(query_or_file) as f:
             query = f.read()
     else:
         query = query_or_file
-    print(f'query is:\n{query}')
     res = fil.sql(query)
     if res is not None:
         for row in res:
--- db_query_pd.py
+++ db_query_pd.py
@@ -3,19 +3,15 @@ import fil
 import sys
 import pandas as pd
 
-for i,arg in enumerate(sys.argv):
-    print(i, arg)
 if len(sys.argv) < 2:
     print("Usage: python db_query.py <query or .sql file>")
 else:
     # Join all the arguments except the script name
     query_or_file = ' '.join(sys.argv[1:])
-    print(f'searching for {query_or_file}')
     if os.path.exists(query_or_file):
         with open(query_or_file) as f:
             query = f.read()
     else:
         query = query_or_file
-    print(f'query is:\n{query}')
     res = fil.sql(query)
     print(pd.DataFrame(res))
