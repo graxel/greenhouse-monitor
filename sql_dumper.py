9f3cbbfe3d0ff3fc559b9fb99d8a1782fb164b78
graxel <grazel98@gmail.com>
2024-06-21 13:36:02 -0400

    added sql_dumper.py

---
+++ sql_dumper.py
@@ -0,0 +1,41 @@
+from flask import Flask, request, jsonify
+import traceback
+import fil
+
+
+app = Flask(__name__)
+
+@app.route('/execute_sql', methods=['POST'])
+def execute_sql():
+    data = request.get_json()
+    sql_command = data.get('sql_command')
+
+    if not sql_command:
+        rows = fil.sql("""
+            select
+                scraped_at,
+                company_name,
+                job_title,
+                location,
+                job_page_url
+            from jobs
+            order by job_id desc
+            ;""")
+
+        # Convert the result to a list of dictionaries
+        results = [dict(row) for row in rows]
+        return jsonify(results), 200
+
+    try:
+        rows = fil.sql(sql_command)
+
+        # Convert the result to a list of dictionaries
+        results = [dict(row) for row in rows]
+        return jsonify(results), 200
+
+    except:
+        error_msg = str(traceback.format_exc())
+        return jsonify({"error": error_msg}), 400
+
+if __name__ == '__main__':
+    app.run(debug=True)
